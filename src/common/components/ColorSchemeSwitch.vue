cac219bf120e019e5b04ae5d206c6f2a8f9949e5
Micha Alt <micha@haus23.net>
2021-05-26 23:29:11 +0200

    Implement a simple color scheme switch.

---
+++ src/common/components/ColorSchemeSwitch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <button @click="enableDarkMode(true)" class="dark:hidden h-6 w-6 flex justify-center items-center focus:outline-none">
+      <MoonIcon />
+    </button>
+    <button @click="enableDarkMode(false)" class="hidden dark:block text-yellow-300  h-6 w-6 flex justify-center items-center focus:outline-none">
+      <SunIcon />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { MoonIcon, SunIcon} from '@heroicons/vue/outline';
+import colorSchemeService from '../../services/color-scheme-service';
+
+export default defineComponent({
+  name: "ColorSchemeSwitch",
+  components: {
+    MoonIcon,
+    SunIcon
+  },
+  methods: {
+    enableDarkMode(enabled: true) {
+      colorSchemeService.setDarkMode((enabled));
+    }
+  }
+})
+</script>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,11 +1,14 @@
 <template>
   <nav class="bg-gray-50 dark:bg-gray-700 text-gray-600 dark:text-gray-200 h-12 leading-8 flex items-center px-2 sm:px-4 gap-x-2 sm:gap-x-4">
-    <div>
+    <div class="flex-grow">
       <a href="/" class="flex gap-x-2 text-gray-800 dark:text-gray-50">
         <img class="w-8 h-8 dark:hidden" src="../assets/logo-dark.svg" alt="Logo">
         <img class="w-8 h-8 hidden dark:block" src="../assets/logo.svg" alt="Logo">
         <span>runde.tips</span>
       </a>
+    </div>
+    <div>
+      <color-scheme-switch></color-scheme-switch>
     </div>
   </nav>
   <main class="container mx-auto px-4 pt-8">
@@ -15,8 +18,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ColorSchemeSwitch from "../common/components/ColorSchemeSwitch.vue";
 
 export default defineComponent({
-  name: 'AppLayout'
+  name: 'AppLayout',
+  components: {ColorSchemeSwitch}
 })
 </script>
