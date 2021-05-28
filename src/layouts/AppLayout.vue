bc19c61c1dcfd73aec7e72fd65219047aa04d70b
Micha Alt <micha@haus23.net>
2021-05-28 16:51:45 +0200

    WIP: start with a responsive header.

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,13 +1,25 @@
 <template>
   <nav class="bg-gray-50 dark:bg-gray-700 text-gray-600 dark:text-gray-200 h-12 leading-8 flex items-center px-2 sm:px-4 gap-x-2 sm:gap-x-4">
-    <div class="flex-grow">
-      <a href="/" class="flex gap-x-2 text-gray-800 dark:text-gray-50">
-        <img class="w-8 h-8 dark:hidden" src="../assets/logo-dark.svg" alt="Logo">
-        <img class="w-8 h-8 hidden dark:block" src="../assets/logo.svg" alt="Logo">
-        <span>runde.tips</span>
-      </a>
+    <div class="flex flex-grow items-center">
+      <div class="flex flex-grow">
+        <a href="/" class="flex gap-x-2 text-gray-800 dark:text-gray-50">
+          <img class="w-8 h-8 dark:hidden" src="../assets/logo-dark.svg" alt="Logo">
+          <img class="w-8 h-8 hidden dark:block" src="../assets/logo.svg" alt="Logo">
+          <span>runde.tips</span>
+        </a>
+      </div>
+      <div class="flex items-center sm:hidden">
+        <button>
+          <menu-icon class="w-6 h-6"></menu-icon>
+        </button>
+      </div>
     </div>
-    <div>
+    <div class="hidden sm:flex w-full items-center">
+      <div>
+        <a href="/tabelle">Tabelle</a>
+        <a href="/tabelle">Spieler</a>
+        <a href="/tabelle">Spiele</a>
+      </div>
       <color-scheme-switch></color-scheme-switch>
     </div>
   </nav>
@@ -17,11 +29,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+
+import { MenuIcon } from '@heroicons/vue/outline';
 import ColorSchemeSwitch from "../common/components/ColorSchemeSwitch.vue";
 
 export default defineComponent({
   name: 'AppLayout',
-  components: {ColorSchemeSwitch}
+  components: {
+    ColorSchemeSwitch,
+    MenuIcon
+  }
 })
 </script>
