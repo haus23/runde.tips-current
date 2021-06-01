c288b526f398d766a683d595f01c6f87e86482f3
Micha Alt <micha@haus23.net>
2021-06-01 23:11:37 +0200

    Add login route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import FohStandings from '../views/foh/Standings.vue';
 import FohPlayers from '../views/foh/Players.vue';
 import FohMatches from '../views/foh/Matches.vue';
+import FohLogIn from '../views/foh/LogIn.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: FohStandings },
   { path: '/tabelle', component: FohStandings },
   { path: '/spieler', component: FohPlayers },
   { path: '/spiele', component: FohMatches },
+  { path: '/login', component: FohLogIn },
 ];
 
 const router = createRouter({
