a6c7eb3d12f2b5be6342c53481e2614206c8b5ee
Micha Alt <micha@haus23.net>
2021-06-04 11:57:13 +0200

    Create login form.

--- src/views/foh/LogIn.vue
+++ src/views/foh/LogIn.vue
@@ -1,11 +1,51 @@
 <template>
-  <h2 class="text-3xl">Log In</h2>
+<div class="flex flex-col items-center">
+    <div class="flex flex-col space-y-8 items-center justify-center max-w-md w-full">
+      <h2 class="text-3xl">Anmeldung</h2>
+      <form class="w-full space-y-4" @submit.prevent="logIn">
+        <div class="rounded-md -space-y-px">
+          <div>
+            <label for="email" class="sr-only">Email</label>
+            <input v-model="email" id="email" type="text" placeholder="Email" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-gray-100 dark:bg-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Passwort</label>
+            <input v-model="password" id="password" type="password" placeholder="Passwort" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-gray-100 dark:bg-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+          </div>
+        </div>
+        <div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 dark:bg-indigo-800 hover:bg-indigo-700 dark:hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+            Anmelden
+          </button>
+        </div>
+      </form>
+    </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import { LockClosedIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
-  name: 'FohLogIn'
-})
+  name: "FohLogIn",
+  components: { LockClosedIcon },
+  setup() {
+    const email = ref('');
+    const password = ref('');
+
+    const logIn = () => {
+
+    };
+
+    return {
+      email,
+      password,
+      logIn
+    };
+  },
+});
 </script>
