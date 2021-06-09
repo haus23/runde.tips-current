474106250da054f75ac578991a06bd07b0ff077d
Micha Alt <micha@haus23.net>
2021-06-09 11:18:30 +0200

    Implement a splash screen displayed while loading data.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
+  <transition-root :show="isLoading"
+    leave="transition-opacity duration-300"
+    leave-from="opacity-100"
+    leave-to="opacity-0"
+  >
+    <splash-screen></splash-screen>
+  </transition-root>
   <app-layout>
     <router-view></router-view>
   </app-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { TransitionRoot } from '@headlessui/vue';
 import AppLayout from './layouts/AppLayout.vue';
+import SplashScreen from './common/components/SplashScreen.vue';
 import { useStore } from './store';
 
 export default defineComponent({
   name: 'App',
   components: {
-    AppLayout
+    AppLayout,
+    TransitionRoot,
+    SplashScreen
   },
   setup() {
-    useStore().dispatch('fetchInitialData');
+    const store = useStore();
+
+    setTimeout(() => {
+      store.dispatch('fetchInitialData');
+    }, 3000);
+
+    return {
+      isLoading: computed(() => store.state.isLoading)
+    }
   }
 })
 </script>
---
+++ src/common/components/SplashScreen.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="absolute text-gray-700 font-semibold bg-gray-50 min-h-screen flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center gap-y-6">
+      <span class="text-4xl">runde.tips</span>
+      <img class="w-2/5 sm:w-1/6" src="../../assets/logo-dark.svg" alt="Logo">
+      <div class="shim relative overflow-hidden w-2/3 sm:w-1/2 h-2 bg-gray-200 rounded">
+        <div class="w-full h-full bg-indigo-500 rounded"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({});
+</script>
+
+<style>
+.shim-red {
+  position: relative;
+  overflow: hidden;
+  background-color: rgba(255, 0, 0, 0.7);
+}
+.shim::after {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(0%);
+  background-image: linear-gradient(
+    90deg,
+    rgba(233, 233, 233, 1) 0,
+    rgba(233, 233, 233, 0.9) 50%,
+    rgba(233, 233, 233, 0.8) 100%
+  );
+  animation: shimmer 3s ease-out infinite;
+  content: "";
+}
+
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+}
+
+</style>
