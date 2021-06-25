c3f3475c8226e9aafd1d76acfe94b3be12821254
Micha Alt <micha@haus23.net>
2021-06-25 20:57:29 +0200

    WIP: create new player.

---
+++ src/modules/backyard/components/PlayerForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <h3 class="text-xl font-semibold border-b">Neuer Spieler</h3>
+  <form @submit.prevent="save">
+    <div>
+      <label for="name">Name</label
+      ><input id="name" type="text" placeholder="Name" />
+    </div>
+    <div>
+      <label for="email">Email</label
+      ><input id="email" type="email" placeholder="Name" />
+    </div>
+    <div>
+      <button
+        class="
+          bg-pink-500
+          text-white
+          active:bg-pink-600
+          font-bold
+          uppercase
+          text-sm
+          px-6
+          py-3
+          rounded
+          shadow
+          hover:shadow-lg
+          outline-none
+          focus:outline-none
+          mr-1
+          mb-1
+          ease-linear
+          transition-all
+          duration-150
+        "
+        type="submit"
+      >
+        Speichern
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+  emits: ['save'],
+  setup(props, context) {
+    const name = ref('');
+    const email = ref('');
+
+    return {
+      name,
+      email,
+      save() {
+        context.emit('save', { name, email });
+      },
+    };
+  },
+};
+</script>
--- src/views/backyard/ChampionshipPlayers.vue
+++ src/views/backyard/ChampionshipPlayers.vue
@@ -16,7 +16,7 @@
     "
   >
     <div>
-      <h3>Turnier</h3>
+      <h3 class="uppercase font-semibold mb-2">Turnier</h3>
       <ul>
         <li v-for="cp in championshipPlayers" :key="cp.id">
           {{ players[cp.player.id].name }}
@@ -24,20 +24,53 @@
       </ul>
     </div>
     <div>
-      <h3>Spieler</h3>
+      <h3 class="uppercase font-semibold mb-2">Spieler</h3>
       <ul>
         <li v-for="(p, id) in players" :key="id">{{ p.name }}</li>
       </ul>
     </div>
+    <div>
+      <button
+        class="
+          bg-indigo-500
+          text-white
+          active:bg-indigo-600
+          font-bold
+          uppercase
+          text-xs
+          px-4
+          py-2
+          rounded
+          shadow
+          hover:shadow-md
+          outline-none
+          focus:outline-none
+          mr-1
+          mb-1
+          ease-linear
+          transition-all
+          duration-150
+        "
+        type="button"
+        @click="isOpen = true"
+      >
+        Neu ...
+      </button>
+    </div>
   </div>
+  <app-dialog :show="isOpen"></app-dialog>
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core';
+import { computed, ref } from '@vue/runtime-core';
 import { useStore } from 'vuex';
 import { useCollection, useDictionary } from '../../api/firebase';
+import AppDialog from '../../common/components/AppDialog.vue';
+
 export default {
+  components: { AppDialog },
   setup() {
+    const isOpen = ref(false);
     const store = useStore();
 
     const championship = computed(() => store.state.backyard.championship);
@@ -53,6 +86,7 @@ export default {
       championship,
       players,
       championshipPlayers,
+      isOpen,
     };
   },
 };
