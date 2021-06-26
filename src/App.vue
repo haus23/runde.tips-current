093d8c49219ce37be8537941096478f1b310fe01
Micha Alt <micha@haus23.net>
2021-06-26 21:43:11 +0200

    Turn app into a vue app.

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-gray-700 text-2xl font-semibold mt-16 text-center">runde.tips</h1>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
 // Styles
 import './styles.css';
+
+// App
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
