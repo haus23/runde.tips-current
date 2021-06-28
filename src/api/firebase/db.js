d1da9b9d84564c43cb8890c6d6cdc0b4e25709cd
Micha Alt <mail@lean-stack.de>
2021-06-28 22:50:42 +0200

    Add first helper and composition utilities.

--- src/api/firebase/db.js
+++ src/api/firebase/db.js
@@ -1,4 +1,28 @@
-import { getFirestore } from 'firebase/firestore';
+import { onUnmounted, ref } from 'vue';
+import { getDocs, getFirestore, collection, onSnapshot } from 'firebase/firestore';
 import { firebaseApp } from '@/api/firebase/app';
 
 export const db = getFirestore(firebaseApp);
+
+// Helper
+export async function getCollection(...path) {
+  const collectionRef = collection(db, path.join('/'));
+  const snapshot = await getDocs(collectionRef);
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+}
+
+// Composition utilities
+export function useCollection(...path) {
+  const data = ref([]);
+
+  const collectionRef = collection(db, path.join('/'));
+  const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
+    data.value = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  });
+
+  onUnmounted(() => {
+    unsubscribe();
+  });
+
+  return [data];
+}
