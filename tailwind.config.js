cd86f78fbb2ef671dafedd0112ff2cc3f4ac261b
Micha Alt <mail@lean-stack.de>
2021-06-29 10:26:25 +0200

    Adjust tailwind config to look into typescript files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: process.env.NODE_ENV ? 'jit' : undefined,
-  purge: ['./index.html', './src/**/*.{vue,js}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts}'],
   darkMode: 'class',
   theme: {
     extend: {},
