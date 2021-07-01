bd0e855ec53cbd51d55466bcd01efab8b55cb60b
Micha Alt <micha@haus23.net>
2021-07-01 13:25:07 +0200

    Prepare tailwind ui.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
