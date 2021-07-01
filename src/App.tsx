af49f8d4db350d324b6c63c4eece35ff0cbbed05
Micha Alt <micha@haus23.net>
2021-07-01 17:02:34 +0200

    Add a loading splash screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
+
+import { Transition } from '@headlessui/react';
+
 import { AppLayout } from './components/AppLayout';
-import { LogIn, Matches, Players, Standings } from './pages';
+import { SplashScreen } from './components/SplashScreen';
 import { Backyard } from './backyard/Backyard';
 
+import { LogIn, Matches, Players, Standings } from './pages';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 3000);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/hinterhof">
-          <Backyard />
-        </Route>
-        <Route>
-          <AppLayout>
-            <Switch>
-              <Route path="/tabelle">
-                <Standings />
-              </Route>
-              <Route path="/spieler">
-                <Players />
-              </Route>
-              <Route path="/spiele">
-                <Matches />
-              </Route>
-              <Route path="/login">
-                <LogIn />
-              </Route>
-              <Route path="/">
-                <Redirect to="/tabelle" />
-              </Route>
-            </Switch>
-          </AppLayout>
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Transition
+        show={isLoading}
+        leave="transition-opacity duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <SplashScreen />
+      </Transition>
+      <Router>
+        <Switch>
+          <Route path="/hinterhof">
+            <Backyard />
+          </Route>
+          <Route>
+            <AppLayout>
+              <Switch>
+                <Route path="/tabelle">
+                  <Standings />
+                </Route>
+                <Route path="/spieler">
+                  <Players />
+                </Route>
+                <Route path="/spiele">
+                  <Matches />
+                </Route>
+                <Route path="/login">
+                  <LogIn />
+                </Route>
+                <Route path="/">
+                  <Redirect to="/tabelle" />
+                </Route>
+              </Switch>
+            </AppLayout>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
